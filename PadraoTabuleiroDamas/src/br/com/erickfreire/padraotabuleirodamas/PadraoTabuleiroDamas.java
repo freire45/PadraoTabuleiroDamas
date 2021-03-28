1248a563dd073f85465e0670bb71cf1296bcfbb9
Erick Freire <erilefr@gmail.com>
2021-03-27 22:55:26 -0300

    PadraoTabuleiroDamas
    PadraoTabuleiroDamas

---
+++ PadraoTabuleiroDamas/src/br/com/erickfreire/padraotabuleirodamas/PadraoTabuleiroDamas.java
@@ -0,0 +1,22 @@
+package br.com.erickfreire.padraotabuleirodamas;
+
+
+/**
+ * Crie um programa em Java que crie um padrão de tabuleiro de damas com asterisco
+ * @author EriCK Freire
+ * @version 1 - Criado em 27-03/2021 as 22:52
+ */
+
+public class PadraoTabuleiroDamas {
+	public static void main(String[] args) {
+		System.out.printf("* * * * * * * *%n"
+				        + " * * * * * * * *%n"
+				        + "* * * * * * * *%n"
+				        + " * * * * * * * *%n"
+				        + "* * * * * * * * %n"
+				        + " * * * * * * * *%n"
+				        + "* * * * * * * * %n"
+				        + " * * * * * * * *%n");
+	}
+
+}
